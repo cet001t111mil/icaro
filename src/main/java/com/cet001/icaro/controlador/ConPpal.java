7b13c828096d2c4ead9d5365b246c2fc537a73e0
ponsanti <35336181+ponsanti@users.noreply.github.com>
2018-06-26 19:11:01 -0300

    Avance MVC multi listener

--- src/main/java/com/cet001/icaro/controlador/ConPpal.java
+++ src/main/java/com/cet001/icaro/controlador/ConPpal.java
@@ -8,11 +8,11 @@ package com.cet001.icaro.controlador;
 import java.awt.event.ActionListener;
 import com.cet001.icaro.vista.Principal;
 import com.cet001.icaro.vista.NuevoChofer;
+import com.cet001.icaro.vista.NuevoCliente;
 import com.cet001.icaro.vista.NuevoViaje;
 import java.awt.event.ActionEvent;
 import javax.swing.JButton;
 import javax.swing.JMenuItem;
-
 
 /**
  *
@@ -20,17 +20,15 @@ import javax.swing.JMenuItem;
  */
 public class ConPpal implements ActionListener {
 
-    
     private Principal wPrincipal;
-    
 
     public ConPpal(Principal wPrincipal) {
-        
+
         this.wPrincipal = wPrincipal;
         this.wPrincipal.menuAgregarChofer.addActionListener(this);
         this.wPrincipal.jMenuItem1.addActionListener(this);
-        
-        
+        this.wPrincipal.agreCliente.addActionListener(this);
+
     }
 
     public void iniciar() {
@@ -39,15 +37,15 @@ public class ConPpal implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        
+
         String opcion = e.getActionCommand();
-                
+
         switch (opcion) {
             case "nChof": {
                 NuevoChofer nChof = new NuevoChofer();
                 nChof.setTitle("Nuevo Chofer");
                 nChof.setVisible(true);
-                ConNChof connchof = new ConNChof (nChof);
+                ConNChof connchof = new ConNChof(nChof);
                 break;
             }
             case "nviaje": {
@@ -55,9 +53,14 @@ public class ConPpal implements ActionListener {
                 NuevoViaje nVven = new NuevoViaje();
                 nVven.setTitle("Nuevo Viaje");
                 nVven.setVisible(true);
-                
-               
-                
+                break;
+            }
+            case "agreCliente": {
+                System.out.println("nuevo cliente");
+                NuevoCliente nVclien = new NuevoCliente();
+                nVclien.setTitle("Nuevo Cliente");
+                nVclien.setVisible(true);
+
             }
         }
 
--- src/main/java/com/cet001/icaro/vista/Principal.java
+++ src/main/java/com/cet001/icaro/vista/Principal.java
@@ -30,13 +30,27 @@ public class Principal extends javax.swing.JFrame {
     private void initComponents() {
 
         jButton1 = new javax.swing.JButton();
+        jSeparator1 = new javax.swing.JSeparator();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jList1 = new javax.swing.JList<>();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jList2 = new javax.swing.JList<>();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu2 = new javax.swing.JMenu();
         jMenuItem1 = new javax.swing.JMenuItem();
+        jMenuItem7 = new javax.swing.JMenuItem();
         menuChofer = new javax.swing.JMenu();
         menuAgregarChofer = new javax.swing.JMenuItem();
+        jMenuItem6 = new javax.swing.JMenuItem();
         jMenu1 = new javax.swing.JMenu();
+        jMenuItem5 = new javax.swing.JMenuItem();
         jMenu4 = new javax.swing.JMenu();
+        agreCliente = new javax.swing.JMenuItem();
+        jMenuItem3 = new javax.swing.JMenuItem();
+        jMenuItem2 = new javax.swing.JMenuItem();
+        jMenuItem4 = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -46,6 +60,14 @@ public class Principal extends javax.swing.JFrame {
                 jButton1ActionPerformed(evt);
             }
         });
+
+        jScrollPane1.setViewportView(jList1);
+
+        jScrollPane2.setViewportView(jList2);
+
+        jLabel1.setText("Vehiculos Disponibles:");
+
+        jLabel2.setText("Viajes En Curso:");
 
         jMenu2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/viaje.png"))); // NOI18N
 
@@ -57,6 +79,9 @@ public class Principal extends javax.swing.JFrame {
             }
         });
         jMenu2.add(jMenuItem1);
+
+        jMenuItem7.setText("Cancelar Viaje...");
+        jMenu2.add(jMenuItem7);
 
         jMenuBar1.add(jMenu2);
 
@@ -76,12 +101,48 @@ public class Principal extends javax.swing.JFrame {
         });
         menuChofer.add(menuAgregarChofer);
 
+        jMenuItem6.setText("Calcular Comisiones...");
+        jMenuItem6.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem6ActionPerformed(evt);
+            }
+        });
+        menuChofer.add(jMenuItem6);
+
         jMenuBar1.add(menuChofer);
 
         jMenu1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/operador.png"))); // NOI18N
+
+        jMenuItem5.setText("Agregar Operador...");
+        jMenu1.add(jMenuItem5);
+
         jMenuBar1.add(jMenu1);
 
         jMenu4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/pasajero2.png"))); // NOI18N
+
+        agreCliente.setText("Agregar Cliente...");
+        agreCliente.setActionCommand("agreCliente");
+        jMenu4.add(agreCliente);
+
+        jMenuItem3.setText("Eliminar Cliente...");
+        jMenuItem3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem3ActionPerformed(evt);
+            }
+        });
+        jMenu4.add(jMenuItem3);
+
+        jMenuItem2.setText("Agregar Saldo...");
+        jMenuItem2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem2ActionPerformed(evt);
+            }
+        });
+        jMenu4.add(jMenuItem2);
+
+        jMenuItem4.setText("Eliminar Saldo...");
+        jMenu4.add(jMenuItem4);
+
         jMenuBar1.add(jMenu4);
 
         setJMenuBar(jMenuBar1);
@@ -90,15 +151,36 @@ public class Principal extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(577, Short.MAX_VALUE)
-                .addComponent(jButton1)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGap(0, 567, Short.MAX_VALUE)
+                        .addComponent(jButton1))
+                    .addComponent(jSeparator1)
+                    .addComponent(jScrollPane1)
+                    .addComponent(jScrollPane2)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel2)
+                            .addComponent(jLabel1))
+                        .addGap(0, 0, Short.MAX_VALUE)))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(394, Short.MAX_VALUE)
+                .addContainerGap(27, Short.MAX_VALUE)
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(23, 23, 23)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
                 .addComponent(jButton1)
                 .addContainerGap())
         );
@@ -122,6 +204,18 @@ public class Principal extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
 this.dispose();    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jMenuItem2ActionPerformed
+
+    private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jMenuItem3ActionPerformed
+
+    private void jMenuItem6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem6ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jMenuItem6ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -160,12 +254,26 @@ this.dispose();    }//GEN-LAST:event_jButton1ActionPerformed
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    public javax.swing.JMenuItem agreCliente;
     private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JList<String> jList1;
+    private javax.swing.JList<String> jList2;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenu jMenu4;
     private javax.swing.JMenuBar jMenuBar1;
     public javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuItem jMenuItem3;
+    private javax.swing.JMenuItem jMenuItem4;
+    private javax.swing.JMenuItem jMenuItem5;
+    private javax.swing.JMenuItem jMenuItem6;
+    private javax.swing.JMenuItem jMenuItem7;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JSeparator jSeparator1;
     public javax.swing.JMenuItem menuAgregarChofer;
     public javax.swing.JMenu menuChofer;
     // End of variables declaration//GEN-END:variables
